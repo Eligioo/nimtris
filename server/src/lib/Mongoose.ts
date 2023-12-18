7da9ad7fd496c691cbc33f08f6a4e39ee55a6fdb
Stefan <stefan@novabytes.nl>
2023-12-18 16:56:57 +0100

    Switch to ip instead of localhost for mongo

--- server/src/lib/Mongoose.ts
+++ server/src/lib/Mongoose.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default class Mongoose {
   public static async connect() {
     try {
-      await mongoose.connect('mongodb://localhost/nimtrix');
+      await mongoose.connect('mongodb://127.0.0.1/nimtrix');
       console.log("Connected with database...")
     } catch (error) {
       console.log(error)
